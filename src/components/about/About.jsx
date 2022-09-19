dc8b6ed1aa3a19a8904e4378bb86de0bd935a400
Edberaga <edbetjonnathan@gmail.com>
2022-09-20 04:59:49 +0800

    Add Responsiveness

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,22 +11,22 @@ const About = () => {
                 <h1>My Projects</h1>
             </div>
             <div className="a-img-wrapper">
-                <div className="project">
+                <div className="image">
                     <h1 className="p-title">School Admission</h1>
                     <img src={Adm} alt="" className='p-img' />
                     <p className="p-desc">Build A school Admission Poster for the peace education centre school</p>
                 </div>
-                <div className="project">
+                <div className="image">
                     <h1 className="p-title">TPEC Logo</h1>
                     <img src={logo} alt="" className='p-img' />
                     <p className="p-desc">Create a logo for the peace education centre school</p>
                 </div>
-                <div className="project">
+                <div className="image">
                     <h1 className="p-title">School Poster</h1>
                     <img src={Poster} alt="" className='p-img' />
                     <p className="p-desc">Build A school Poster for the peace education centre school</p>
                 </div>
-                <div className="project">
+                <div className="image">
                     <h1 className="p-title">Birthday Card</h1>
                     <img src={Nicholas} alt="" className='p-img' />
                     <p className="p-desc">Build A school Admission Poster for the peace education centre school</p>
