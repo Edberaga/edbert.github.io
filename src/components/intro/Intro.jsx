5c9e5b7e46ac22c04247c13b68ef0d05393486ce
Edberaga <edbetjonnathan@gmail.com>
2024-02-08 18:11:57 +0700

    Change the intro description

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -6,18 +6,17 @@ const Intro = () => {
         <div className="i">
             <div className="i-left">
                 <div className="i-left-wrapper">
-                    <h2 className="i-intro">Hello, My name is</h2>
+                    <h1 className="i-intro">Hello, My name is</h1>
                     <h1 className="i-name">Jonnathan Edbert Halim</h1>
                     <div className="i-title">
                         <div className="i-title-wrapper">
                             <div className="i-title-item">Web Developer</div>
-                            <div className="i-title-item">Graphic Designer</div>
-                            <div className="i-title-item">UI/UX Designer</div>
+                            <div className="i-title-item">Full Stack Programmer</div>
+                            <div className="i-title-item">Web Designer</div>
                         </div>
                     </div>
                     <div className='i-desc'>
-                        I'm a IT Student who passionate to creating stylish images,
-                        modern websites, and web services. 
+                    <p>My journey in Programming started with a curiosity for coding and a desire to provide impactful life solutions.</p>
                     </div>
                 </div>
             </div>
