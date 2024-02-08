027bbc5a517c412f18ee9a7c7339d19aa5225190
Edberaga <edbetjonnathan@gmail.com>
2024-02-08 18:11:41 +0700

    My projects data

---
+++ src/data/WorkProject.js
@@ -0,0 +1,52 @@
+import saysheji from '../img/website/saysheji.webp'
+import thaigood4u from '../img/website/thaigood4u.webp'
+import msiagood4u from '../img/website/msiagood4u.webp'
+import vdo from '../img/website/vdosmartgadget.webp'
+import chiangMai from '../img/website/chiangmaipropertyinvest.webp'
+import ssj_crm from '../img/website/saysheji-dashboard.webp'
+
+export const WorkProject = [
+    {
+        "name": "Saysheji",
+        "link": "https://saysheji.org/",
+        "img": saysheji,
+        "desc": "My Company Personal Website to showcase our projects and services",
+        "type": "website"
+    },
+    {
+        "name": "Vdo Smart Widget",
+        "link": "https://vdosmartwidget.com/",
+        "img": vdo,
+        "desc": "WooCommerce site that sells brands of digital media devices.",
+        "type": "website"
+    },
+    {
+        "name": "Chiang Mai Property Invest",
+        "link": "https://chiangmaipropertyinvest.com/",
+        "img": chiangMai,
+        "desc": "Website for Searching or Booking property places in Chiang Mai",
+        "type": "website"
+    },
+    {
+        "name": "Saysheji Dashboard",
+        "link": "https://crm-by-ssj.web.app/",
+        "img": ssj_crm,
+        "desc": "My Personal Project for the company dashboard web appliction",
+        "type": "web_app",
+        "github": "https://github.com/Edberaga/Saysheji-Dashboard"
+    },
+    {
+        "name": "ThaiGood4u",
+        "link": "https://thaigood4u.com/",
+        "img": thaigood4u,
+        "desc": "A Thailand organization to assist F&B or Tourism Business to expand their reach",
+        "type": "website"
+    },
+    {
+        "name": "MsiaGood4u",
+        "link": "https://msigood4u.com/",
+        "img": msiagood4u,
+        "desc": "A Malaysia organization to assist F&B or Tourism Business to expand their reach",
+        "type": "website"
+    },
+]
