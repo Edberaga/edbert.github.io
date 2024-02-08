1beb228e72c19dbee3442fc68626328c4537648c
Edberaga <edbetjonnathan@gmail.com>
2024-02-08 18:12:28 +0700

    Make the Project component to be dynamic

--- src/components/list/Product.jsx
+++ src/components/list/Product.jsx
@@ -1,74 +1,39 @@
-import KD from '../../img/website/kareducation.png'
-import NFT from '../../img/website/NFT.png'
-import NIU from '../../img/website/NIU.png'
-import EU from '../../img/website/eu-website.png'
-import POR from '../../img/website/Portugal.png'
-import LAT from '../../img/website/Latvia.png'
+import React from "react"
+import { WorkProject } from "../../data/WorkProject"
 
 export const Product = ({img, link}) => {
     return (
-        <>
-        <div className="p">
-            <div className="p-browser">
-                <div className="p-circle"></div>
-                <div className="p-circle"></div>
-                <div className="p-circle"></div>
-            </div>
-            <a href="https://kareducation.com/" target="_blank" rel="noreferrer">
-                <img src={KD} alt="" className="p-image"/>
-            </a>
-        </div>
-        <div className="p">
-            <div className="p-browser">
-                <div className="p-circle"></div>
-                <div className="p-circle"></div>
-                <div className="p-circle"></div>
-            </div>
-            <a href='https://kareducation.com/artist/' target="_blank" rel="noreferrer">
-                <img src={NFT} alt="" className="p-image"/>
-            </a>
-        </div>
-        <div className="p">
-            <div className="p-browser">
-                <div className="p-circle"></div>
-                <div className="p-circle"></div>
-                <div className="p-circle"></div>
-            </div>
-            <a href='https://kareducation.com/niu/' target="_blank" rel="noreferrer">
-                <img src={NIU} alt="" className="p-image"/>
-            </a>
-        </div>
+      <>
+        {WorkProject.map((par, index) => {
+        return (
+            <div key={index} className='p-container'>
+                <div className="p">
+                    <div className="p-browser">
+                        <div className="p-circle"></div>
+                        <div className="p-circle"></div>
+                        <div className="p-circle"></div>
+                    </div>
+                    <h2>{par.name}</h2>
+                    <img src={par.img} alt={par.name} className='p-image'/>
+                </div>
 
-        <div className="p">
-            <div className="p-browser">
-                <div className="p-circle"></div>
-                <div className="p-circle"></div>
-                <div className="p-circle"></div>
+                <div className="p-description">
+                    <p>
+                        {par.desc}
+                    </p>
+                    <div className="p-buttons">
+                        <button className='p-button'>
+                            <a href={par.link} target="_blank" rel="noreferrer">Visit Site</a>
+                        </button>
+                        {(par.type === "web_app") && 
+                        <button className='p-button'>
+                            <a href={par.github} target="_blank" rel="noreferrer">View Code</a>
+                        </button>
+                        }
+                    </div>
+                </div>
             </div>
-            <a href='https://euequities.io/' target="_blank" rel="noreferrer">
-                <img src={EU} alt="" className="p-image"/>
-            </a>
-        </div>
-        <div className="p">
-            <div className="p-browser">
-                <div className="p-circle"></div>
-                <div className="p-circle"></div>
-                <div className="p-circle"></div>
-            </div>
-            <a href='https://euequities.io/portugal/' target="_blank" rel="noreferrer">
-                <img src={POR} alt="" className="p-image"/>
-            </a>
-        </div>
-        <div className="p">
-            <div className="p-browser">
-                <div className="p-circle"></div>
-                <div className="p-circle"></div>
-                <div className="p-circle"></div>
-            </div>
-            <a href='https://euequities.io/latvia/' target="_blank" rel="noreferrer">
-                <img src={LAT} alt="" className="p-image"/>
-            </a>
-        </div>
+        );})}
     </>
     )
 }
