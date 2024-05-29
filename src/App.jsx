fc4f7897ff7ce6a1d8ff93dde59854276831304d
Edberaga <edbetjonnathan@gmail.com>
2024-05-30 00:12:59 +0700

    Add section tag as container

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import About from "./components/about/About";
-import { Education } from "./components/education/Education";
+
 import Intro from "./components/intro/Intro";
 import { List } from "./components/list/List";
 
-
 const App = () => {
     return (
-        <div>
-            <Intro/>
-            <Education/>
-            <List/>
-            <About/>
-        </div>
+        <main>
+            <section>
+                <Intro/>
+            </section>
+
+            <section>
+                <List/>
+            </section>
+        </main>
     );
 };
 
